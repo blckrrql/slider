79d0edfab6f80cb57fedb89064c205a4a6598576
Valeriia Sitka <111923324+blckrrql@users.noreply.github.com>
2022-12-06 20:10:48 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,18 @@
+let offset = 0;
+const sliderLine = document.querySelector(".slider-line");
+
+document.querySelector(".slider-next").addEventListener("click", function() {
+    offset = offset + 300;
+    if (offset > 900) {
+        offset = 0;
+    }
+    sliderLine.style.left = -offset + 'px';
+});
+
+document.querySelector(".slider-prev").addEventListener("click", function() {
+    offset = offset - 300;
+    if (offset < 0) {
+        offset = 900;
+    }
+    sliderLine.style.left = -offset + 'px';
+});
